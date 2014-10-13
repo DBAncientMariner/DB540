b3d8363a8d16677e692e9f944b8c6eb71523c2b5
rkyadav-ncsu <rkyadav@ncsu.edu>
2014-10-13 00:38:01 -0400

    added application landing screen and login screen.

--- DBMS_540_Grp_Project/src/ncsu/dbms/core/OracleDb.java
+++ DBMS_540_Grp_Project/src/ncsu/dbms/core/OracleDb.java
@@ -4,6 +4,7 @@
 package ncsu.dbms.core;
 import java.io.*;
 import java.sql.*;
+
 import oracle.jdbc.driver.*;
 
 
@@ -13,35 +14,66 @@ import oracle.jdbc.driver.*;
  */
 public class OracleDb {
 	
-	
+	private String UserName="";
+	private String Password="";
+	private String Url="";
+	private Connection Conn;
+	ResultSet ResultSet;
+	Statement Stmt;
 	public OracleDb(){
+		UserName="SYSTEM";
+		Password="aCZ6q7F";
+		Url="jdbc:oracle:thin:System@//AM-THINKPAD:1521/xe";
+		
+	}
+	public boolean OpenConnection()
+	{
+		try
+		{
+			if(Conn==null ||  Conn.isClosed())
+			{
+				try {
+					Class.forName("oracle.jdbc.OracleDriver");
+				} catch (ClassNotFoundException e) {
+				}
+				Conn = DriverManager.getConnection(Url, UserName, Password);
+			}
+		}
+		catch(SQLException e)
+		{
+			return false;
+		}
+		return true;
+	}
+	public void CloseConnection()
+	{
 		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-		} catch (ClassNotFoundException e) {
+			ResultSet.close();
+			Stmt.close();
+			Conn.close();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String username = "SYSTEM";
-		String password = "aCZ6q7F";
-		String url="jdbc:oracle:thin:System@//AM-THINKPAD:1521/xe";
-		
-		
-		try {
-			Connection conn = DriverManager.getConnection(url, username, password);
-			//STEP 4: Execute a query
-		     Statement stmt = conn.createStatement();
-		      String sql;
-		      sql = "SELECT * FROM CSC_User";
-		      ResultSet rs = stmt.executeQuery(sql);
-		     
-		      rs.close();
-		      stmt.close();
-		      conn.close();
-		} 
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	}
+	public ResultSet GetResultSet(String sql)
+	{
+		ResultSet=null;
+		if(OpenConnection())
+		{
+			try
+			{
+				Stmt= Conn.createStatement();
+				ResultSet = Stmt.executeQuery(sql);
+			}
+			catch(SQLException e)
+			{
+			}
 		}
+		return ResultSet;
+	}
+	public void InsertQuery(String query)
+	{
 		
 	}
 }
--- DBMS_540_Grp_Project/src/ncsu/dbms/ui/Login.java
+++ DBMS_540_Grp_Project/src/ncsu/dbms/ui/Login.java
@@ -3,6 +3,7 @@ package ncsu.dbms.ui;
 import ncsu.dbms.core.*;
 
 import java.awt.EventQueue;
+import java.awt.Window;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,14 +16,15 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLayeredPane;
 
 public class Login {
 
 	private JFrame frame;
-	private JLabel lbl_Welcome;
-	private JTextField txt_LoginName;
-	private JTextField txt_Password;
-
+	private OracleDb Oracle;
+	private JFrame frameStart;
+	static Login window;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -30,8 +32,8 @@ public class Login {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login window = new Login();
-					window.frame.setVisible(true);
+					window = new Login();
+					window.frameStart.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -43,35 +45,43 @@ public class Login {
 	 * Create the application.
 	 */
 	public Login() {
-		initialize();
+		//initialize();
+		initializeFrameStart();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	//this is main login screen
 	private void initialize() {
-		OracleDb oracle=new OracleDb();
+		Oracle=new OracleDb();
 		frame = new JFrame();
 		frame.setResizable(false);
 		frame.setBounds(100, 100, 627, 433);
+		frame.setLocationRelativeTo(null); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		lbl_Welcome = new JLabel("Welcome CSC 505");
+			
+		JLabel lbl_Welcome = new JLabel("Welcome CSC 505");
 		lbl_Welcome.setBounds(173, 13, 172, 50);
 		frame.getContentPane().add(lbl_Welcome);
 		
-		txt_LoginName = new JTextField();
+		final JTextField txt_LoginName = new JTextField();
 		txt_LoginName.setToolTipText("Login Name");
 		txt_LoginName.setBounds(293, 87, 116, 22);
 		frame.getContentPane().add(txt_LoginName);
 		txt_LoginName.setColumns(10);
 		
-		txt_Password = new JTextField();
+		JTextField txt_Password = new JTextField();
 		txt_Password.setToolTipText("Password");
 		txt_Password.setBounds(293, 135, 116, 22);
 		frame.getContentPane().add(txt_Password);
 		txt_Password.setColumns(10);
+		
+		final JLabel lbl_ErrorMessage = new JLabel("");
+		lbl_ErrorMessage.setForeground(Color.RED);
+		lbl_ErrorMessage.setBounds(52, 242, 506, 115);
+		frame.getContentPane().add(lbl_ErrorMessage);
 		
 		JLabel lbl_LoginName = new JLabel("Login Name");
 		lbl_LoginName.setBounds(183, 80, 74, 36);
@@ -84,14 +94,82 @@ public class Login {
 		JButton btn_Login = new JButton("Login");
 		btn_Login.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				lbl_ErrorMessage.setText("");
+				User user=new User();
+				user.GetUser(txt_LoginName.getText());
+				if(user.UserId==0)
+				{
+					lbl_ErrorMessage.setText("No such user exists.");
+				}
+				else
+					lbl_ErrorMessage.setText(user.UserFName);
 			}
 		});
 		btn_Login.setBounds(293, 180, 97, 25);
 		frame.getContentPane().add(btn_Login);
 		
-		JLabel lbl_ErrorMessage = new JLabel("");
-		lbl_ErrorMessage.setForeground(Color.RED);
-		lbl_ErrorMessage.setBounds(52, 242, 506, 115);
-		frame.getContentPane().add(lbl_ErrorMessage);
+		//back button
+		JButton btn_LoginBack = new JButton("Back");
+		btn_LoginBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				initializeFrameStart();
+				window.frameStart.setVisible(true);
+				window.frame.setVisible(false);
+			}
+		});
+		btn_LoginBack.setBounds(180, 180, 97, 25);
+		frame.getContentPane().add(btn_LoginBack);
+		//initializeFrameStart();
+	}	
+
+	//this is main opening screen.
+	private void initializeFrameStart()
+	{
+		
+		frameStart=new JFrame();
+		frameStart.setResizable(false);
+		frameStart.setBounds(100,100,627,433);
+		frameStart.setLocationRelativeTo(null); 
+		frameStart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameStart.getContentPane().setLayout(null);
+		
+		
+		JLabel lbl_Welcome = new JLabel("Welcome CSC 505");
+		lbl_Welcome.setBounds(210, 13, 172, 50);
+		frameStart.getContentPane().add(lbl_Welcome);
+		
+		//Login button
+		JButton btn_Login = new JButton("Login");
+		btn_Login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				initialize();
+				window.frame.setVisible(true);
+				window.frameStart.setVisible(false);
+			}
+		});
+		btn_Login.setBounds(250, 100, 140, 25);
+		frameStart.getContentPane().add(btn_Login);
+		
+		
+		//Create User button
+		JButton btn_CreateUser = new JButton("Create User");
+		btn_CreateUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btn_CreateUser.setBounds(250, 150, 140, 25);
+		frameStart.getContentPane().add(btn_CreateUser);
+		
+		
+		//Exit UI
+		JButton btn_Exit = new JButton("Exit");
+		btn_Exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btn_Exit.setBounds(250, 200, 140, 25);
+		frameStart.getContentPane().add(btn_Exit);
 	}
 }

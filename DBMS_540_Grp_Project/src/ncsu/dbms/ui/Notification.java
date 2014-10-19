ee5da1d3b21d96574bd7845231e844f9da85b0a6
rkyadav-ncsu <rkyadav@ncsu.edu>
2014-10-19 18:48:52 -0400

    added notification

---
+++ DBMS_540_Grp_Project/src/ncsu/dbms/ui/Notification.java
@@ -0,0 +1,47 @@
+package ncsu.dbms.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Notification extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Notification() {
+		//Notification frame = new Notification();
+		//frame.setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+	public void HideNotification()
+	{
+		this.setVisible(false);
+	}
+
+}

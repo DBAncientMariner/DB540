13b0821d9f06126bbd342c9a392c945125147a95
Parth Satra <pasatra@ncsu.edu>
2014-10-25 15:31:04 -0400

    Small Changes
    Change-Id: If5ca9139d7a55db142d498739f4c8a50722c3278
    Signed-off-by: Parth Satra <pasatra@ncsu.edu>

--- DBMS_540_Grp_Project/src/ncsu/dbms/core/UserAttempt.java
+++ DBMS_540_Grp_Project/src/ncsu/dbms/core/UserAttempt.java
@@ -18,4 +18,47 @@ public class UserAttempt {
 	Date UA_LASTATTEMPT_DATE =new Date();
 	boolean UA_SUBMITTED =false;
 	double UA_SCORE =0.00;
+	
+	public int getUA_ID() {
+		return UA_ID;
+	}
+	public void setUA_ID(int uA_ID) {
+		UA_ID = uA_ID;
+	}
+	public int getUA_USER_ID() {
+		return UA_USER_ID;
+	}
+	public void setUA_USER_ID(int uA_USER_ID) {
+		UA_USER_ID = uA_USER_ID;
+	}
+	public int getUA_EXERCISE_ID() {
+		return UA_EXERCISE_ID;
+	}
+	public void setUA_EXERCISE_ID(int uA_EXERCISE_ID) {
+		UA_EXERCISE_ID = uA_EXERCISE_ID;
+	}
+	public Date getUA_STARTATTEMPT_DATE() {
+		return UA_STARTATTEMPT_DATE;
+	}
+	public void setUA_STARTATTEMPT_DATE(Date uA_STARTATTEMPT_DATE) {
+		UA_STARTATTEMPT_DATE = uA_STARTATTEMPT_DATE;
+	}
+	public Date getUA_LASTATTEMPT_DATE() {
+		return UA_LASTATTEMPT_DATE;
+	}
+	public void setUA_LASTATTEMPT_DATE(Date uA_LASTATTEMPT_DATE) {
+		UA_LASTATTEMPT_DATE = uA_LASTATTEMPT_DATE;
+	}
+	public boolean isUA_SUBMITTED() {
+		return UA_SUBMITTED;
+	}
+	public void setUA_SUBMITTED(boolean uA_SUBMITTED) {
+		UA_SUBMITTED = uA_SUBMITTED;
+	}
+	public double getUA_SCORE() {
+		return UA_SCORE;
+	}
+	public void setUA_SCORE(double uA_SCORE) {
+		UA_SCORE = uA_SCORE;
+	}
 }

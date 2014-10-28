a42ee00b488a8dda90d448a68d3197c0f5969acc
gargsajal9 <gargsajal9@gmail.com>
2014-10-28 05:35:57 -0400

    no message

--- DBMS_540_Grp_Project/src/ncsu/dbms/core/OracleDataAdapter1.java
+++ DBMS_540_Grp_Project/src/ncsu/dbms/core/OracleDataAdapter1.java
@@ -6,12 +6,16 @@ public class OracleDataAdapter1 {
 	public static boolean InsertIntoUserAttempExercise(int ua_id,int Q_id, int A_id,char is_select)
 	{
 		OracleDb oracleDb = new OracleDb();
-		try{
 			oracleDb.OpenConnection();
-			boolean resultset = oracleDb.InsertQuery("Insert into CSC_USERATTEMPT_EXERCISE(UE_UA_ID,UE_QUESTION_ID ,UE_ANSWER_ID ,UE_ISSELECTED) Values("+ua_id+","+Q_id+","+A_id+",'"+is_select+"');");
+			boolean resultset = oracleDb.InsertQuery("Insert into CSC_USERATTEMPT_EXERCISE(UE_UA_ID,UE_QUESTION_ID ,UE_ANSWER_ID ,UE_ISSELECTED) Values("+ua_id+","+Q_id+","+A_id+",'"+is_select+"')");
 			return resultset;
-		} finally {
-			oracleDb.CloseConnection();
-		}
+	}
+	
+	public static boolean UpdateIntoUserAttempExercise(int ua_id,int Q_id, int A_id,char is_select)
+	{
+		OracleDb oracleDb = new OracleDb();
+			oracleDb.OpenConnection();
+			boolean resultset = oracleDb.InsertQuery("Update CSC_USERATTEMPT_EXERCISE SET UE_ISSELECTED ='"+is_select+"' where UE_QUESTION_ID ="+Q_id+" and UE_ANSWER_ID ="+A_id+" and UE_UA_ID ="+ua_id);
+			return resultset;
 	}
 }

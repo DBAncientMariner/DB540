8f447f56161aab44c3459826e1e45fbb614aa720
rkyadav-ncsu <rkyadav@ncsu.edu>
2014-11-02 13:33:31 -0500

    no message

--- DBMS_540_Grp_Project/src/ncsu/dbms/core/ConnectionDetails.java
+++ DBMS_540_Grp_Project/src/ncsu/dbms/core/ConnectionDetails.java
@@ -3,15 +3,56 @@
  */
 package ncsu.dbms.core;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author ravi
  *
  */
 public class ConnectionDetails {
 	
-		public static String UserName = "SYSTEM";
-		public static String Password = "aCZ6q7F";
-		public static String Url = "jdbc:oracle:thin:System@//AM-THINKPAD:1521/xe";
-
+//		public static String UserName = "SYSTEM";
+//		public static String Password = "aCZ6q7F";
+//		public static String Url = "jdbc:oracle:thin:System@//AM-THINKPAD:1521/xe";
+	
+	public static String UserName = "";
+	public static String Password = "";
+	public static String Url = "";
+		public static void ReadConfig()
+		{
+			BufferedReader reader;
+			try {
+				reader = new BufferedReader(new FileReader("configfile.txt"));
+				String line = null;
+				try {
+					line = reader.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				UserName=line.toString();
+				try {
+					line = reader.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Password=line.toString();
+				try {
+					line = reader.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Url=line.toString();
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		}
 	
 }
--- DBMS_540_Grp_Project/src/ncsu/dbms/core/OracleDb.java
+++ DBMS_540_Grp_Project/src/ncsu/dbms/core/OracleDb.java
@@ -26,6 +26,7 @@ public class OracleDb {
 	Statement Stmt;
 
 	public OracleDb() {
+		ConnectionDetails.ReadConfig();
 		UserName = ConnectionDetails.UserName;
 		Password = ConnectionDetails.Password;
 		Url = ConnectionDetails.Url;
